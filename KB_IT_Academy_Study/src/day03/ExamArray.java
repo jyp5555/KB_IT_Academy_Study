c64e7b2f9a0e1dfcee0297d0e2f1a3cba2aa4999
student <student@M40324>
2023-03-06 09:57:37 +0900

    Day03 Study Array

---
+++ KB_IT_Academy_Study/src/day03/ExamArray.java
@@ -0,0 +1,13 @@
+package day03;
+
+public class ExamArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = new int[3];
+//		arr[3] = 300;
+		System.out.println(arr.length+"");
+		
+
+	}
+}
