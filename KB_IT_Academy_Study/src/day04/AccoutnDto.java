3b29505a345f0a89fd4c16d2cee19c4477fc3025
student <student@M40324>
2023-03-07 11:09:26 +0900

    Review about Class

---
+++ KB_IT_Academy_Study/src/day04/AccoutnDto.java
@@ -0,0 +1,57 @@
+package day04;
+
+public class AccoutnDto {
+
+	private int accountSeq;
+	private String accountNumber;
+	private int balance;
+	private int userSeq;
+	
+	public AccoutnDto() {}
+
+	public AccoutnDto(int accountSeq, String accountNumber, int balance, int userSeq) {
+		super();//없어도 컴파일러가 자동으로 호출
+		this.accountSeq = accountSeq;
+		this.accountNumber = accountNumber;
+		this.balance = balance;
+		this.userSeq = userSeq;
+	}
+
+	public int getAccountSeq() {
+		return accountSeq;
+	}
+
+	public void setAccountSeq(int accountSeq) {
+		this.accountSeq = accountSeq;
+	}
+
+	public String getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(String accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public int getBalance() {
+		return balance;
+	}
+
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+
+	public int getUserSeq() {
+		return userSeq;
+	}
+
+	public void setUserSeq(int userSeq) {
+		this.userSeq = userSeq;
+	}
+
+	@Override
+	public String toString() {
+		return "accountSeq=" + accountSeq + ", accountNumber=" + accountNumber + ", balance=" + balance
+				+ ", userSeq=" + userSeq;
+	}
+}
---
+++ KB_IT_Academy_Study/src/day04/ExamUserDto.java
@@ -0,0 +1,13 @@
+package day04;
+
+public class ExamUserDto {
+
+	public static void main(String[] args) {
+		
+		UserDto user = new UserDto();
+		
+//		toString() 구현 전: day04.UserDto@6f2b958e <-hashCode
+//		toString() 구현 후: User [userSeq=0, name=null, email=null, phone=null, isSleep=false]
+		System.out.println(user);
+	}
+}
