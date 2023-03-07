e69d5ee3866629e800134cb5bc1ffe5e21d37b6a
student <student@M40324>
2023-03-07 11:31:11 +0900

    Study "final"

---
+++ KB_IT_Academy_Study/src/day04/ExamFinal.java
@@ -0,0 +1,13 @@
+package day04;
+
+import java.util.LinkedList;
+
+public class ExamFinal {
+
+	public static void main(String[] args) {
+		final int age = 32;
+//		age = 30; The final local variable age cannot be assigned. It must be blank and not using a compound assignment
+//		final method도 오버라이딩 금지
+//		final class도 상속 불가 클래스
+	}
+}
