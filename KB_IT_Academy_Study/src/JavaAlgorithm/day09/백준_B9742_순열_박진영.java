85d366bdf94de6d9e07aa0c3fd51092a9c5a4add
jyp5555 <jyp2095@sookmyung.ac.kr>
2023-03-14 18:42:18 +0900

    부분해결

--- KB_IT_Academy_Study/src/JavaAlgorithm/day09/백준_B9742_순열_박진영.java
+++ KB_IT_Academy_Study/src/JavaAlgorithm/day09/백준_B9742_순열_박진영.java
@@ -4,16 +4,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.StringTokenizer;
-import java.util.stream.IntStream;
 
 public class 백준_B9742_순열_박진영 {
-	static ArrayList<char[]> ans = new ArrayList<char[]>();
+	static ArrayList<String> ans = new ArrayList<String>();
 	static char[] arr1;
 	static int n1, m;
-	static boolean[] visited;
+	static ArrayList<Boolean> visited = new ArrayList<Boolean>();
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
@@ -24,7 +21,9 @@ public class 백준_B9742_순열_박진영 {
 		arr1 = new char[temp.length()];
 		n1 = temp.length()-1;
 		m = temp.length()-1;
-		visited = new boolean[temp.length()+1];
+		for(int i =0 ;i<temp.length()+1;i++) {
+			visited.add(false);
+		}
 		try {
 			for (int i = 0; i<temp.length();i++) {
 				temp2[i] = temp.charAt(i);
@@ -34,25 +33,32 @@ public class 백준_B9742_순열_박진영 {
 		}
 		
 		getPermut(0,temp2);
-		System.out.println(ans.toString());
+		String str;
+		try {
+			str = ans.get(n-1);
+		}catch(Exception e) {
+			str = "No permutation";
+		}
+		
+		System.out.println(temp+" "+n + " = "+ str );
 	}
 	
 	static void getPermut(int cnt, char[] arr){
-		
+		ArrayList<String> temp = new ArrayList<String>();
+		for(char c : arr) temp.add(""+c);
 		if (cnt == m) {
-			arr1[arr1.length-1] = arr[m];
-			ans.add(arr1);
+			arr1[cnt] = temp.get(visited.indexOf(false)).toCharArray()[0];
+			String str = String.valueOf(arr1);
+			ans.add(str);
 			return;
 		}
 		
 		for (int i = 0; i <= n1; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                arr1[cnt] = arr[i];
-    			System.out.println(arr1);
-
+            if (!visited.get(i)) {
+                visited.set(i, true);
+                arr1[cnt] = temp.get(i).toCharArray()[0];
                 getPermut(cnt + 1, arr);
-                visited[i] = false; // 다시 방문을 풀어줘야지만 한 번 재귀 끝나고 다시 갈 수 있음
+                visited.set(i, false); // 다시 방문을 풀어줘야지만 한 번 재귀 끝나고 다시 갈 수 있음
             }
         }
 	}
